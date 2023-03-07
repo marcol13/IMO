2f97459cba7827b38120ea307108d79b3ac37a9f
Jedrek00 <jedrek.koscielniak@gmail.com>
2023-03-07 20:22:58 +0100

    Extract functions to file

---
+++ 1_zachlanne/matrix.py
@@ -0,0 +1,40 @@
+import re
+import math
+import numpy as np
+
+
+def calculate_distance(start: [int, int], end: [int, int]):
+    return math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
+
+
+def parse_file(path: str):
+    vertices = []
+    
+    with open(path) as f:
+        lines = f.readlines()
+        
+    for line in lines:
+        if not re.match(r"^[A-Z]", line):
+            line = re.sub("\n", "", line)
+            line = list(map(int, line.split(" ")[1:]))
+            vertices.append(line)
+        
+    return np.array(vertices)
+
+
+def create_distance_matrix(vertices: list):
+    matrix = []
+    
+    for start in vertices:
+        temp = []
+        for end in vertices: 
+            temp.append(calculate_distance(start, end))
+        matrix.append(temp)
+        
+    return np.array(matrix)
+
+
+if __name__ == "__main__":
+    vertices = parse_file("1_zachlanne/data/kroa100.tsp")
+    matrix = create_distance_matrix(vertices)
+    print(matrix.shape)
