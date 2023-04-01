e4778415867d04a8f62a3ac7690987e016550285
Jedrek00 <jedrek.koscielniak@gmail.com>
2023-04-01 18:11:32 +0200

    Add working steepest search

--- local_search/main.py
+++ local_search/main.py
@@ -4,105 +4,12 @@ import numpy as np
 from greedy_regret import greedy_regret
 from matrix import parse_file, create_distance_matrix
 from helpers import calculate_path_length, draw_cycles
+from steepest_search import steepest_search
 
 
 KROA_PATH = "./local_search/data/kroa100.tsp"
 KROB_PATH = "./local_search/data/krob100.tsp"
 
-def swap_vertices_between_cycles(cycle_a, cycle_b):
-    changes = get_pairs_for_swap_between_cycles(cycle_a, cycle_b)
-    a = np.array([calculate_distance_diff_for_swap_between_cycles(matrix, cycle_a, cycle_b, changes[i][0], changes[i][1]) for i in range(len(changes))])
-    if np.min(a) >= 0:
-        return 1
-    vertex_a, vertex_b = changes[np.argmin(a)]
-    index_a, index_b = np.where(cycle_a == vertex_a)[0], np.where(cycle_b == vertex_b)[0]
-    cycle_a[index_a], cycle_b[index_b] = cycle_b[index_b], cycle_a[index_a]
-    return np.min(a)
-
-def swap_vertices_inside_cycle(cycle_a, cycle_b):
-    changes_a, changes_b = get_pairs_for_swap_inside_cycles(cycle_a, cycle_b)
-    a = np.array([calculate_distance_diff_for_swap_inside_cycle(matrix, cycle_a, changes_a[i][0], changes_a[i][1]) for i in range(len(changes_a))])
-    b = np.array([calculate_distance_diff_for_swap_inside_cycle(matrix, cycle_b, changes_b[i][0], changes_b[i][1]) for i in range(len(changes_b))])
-    print(np.min(a), np.min(b))
-    if np.min(a) == 0 and np.min(b) == 0:
-        return 1
-    if np.min(a) < 0:
-        vertex_a, vertex_b = changes_a[np.argmin(a)]
-        index_a, index_b = np.where(cycle_a == vertex_a)[0], np.where(cycle_a == vertex_b)[0]
-        cycle_a[index_a], cycle_a[index_b] = cycle_a[index_b], cycle_a[index_a]
-    if np.min(b) < 0:
-        vertex_a, vertex_b = changes_b[np.argmin(b)]
-        index_a, index_b = np.where(cycle_b == vertex_a)[0], np.where(cycle_b == vertex_b)[0]
-        cycle_b[index_a], cycle_b[index_b] = cycle_b[index_b], cycle_b[index_a]
-    return np.min(a) + np.min(b)
-
-def swap_vertices_between_cycles(cycle_a, cycle_b, vertex_a, vertex_b):
-    index_a, index_b = np.where(cycle_a == vertex_a)[0], np.where(cycle_b == vertex_b)[0]
-    cycle_a[index_a], cycle_b[index_b] = cycle_b[index_b], cycle_a[index_a]
-    return np.min(a)
-
-def swap_vertices_inside_cycle(cycle, vertex_a, vertex_b):
-    index_a, index_b = np.where(cycle == vertex_a)[0], np.where(cycle == vertex_b)[0]
-    cycle[index_a], cycle[index_b] = cycle[index_b], cycle[index_a]
-
-def swap_edges_inside_cycle(cycle, vertex_a, vertex_b):
-    index_a, index_b = np.where(cycle == vertex_a)[0][0], np.where(cycle == vertex_b)[0][0]
-    if index_a == 0 and index_b == len(cycle) - 1:
-        cycle[index_a], cycle[index_b] = cycle[index_b], cycle[index_a]
-    cycle[index_a:index_b+1] = cycle[index_a:index_b+1][::-1]
-
-def get_pairs_for_swap_between_cycles(cycle_a, cycle_b):
-    return [(i, j) for i in cycle_a for j in cycle_b]
-
-def get_pairs_for_swap_inside_cycles(cycle_a, cycle_b):
-    return list(combinations(cycle_a, 2)), list(combinations(cycle_b, 2))
-
-def calculate_distance_diff_for_swap_between_cycles(matrix, cycle_a, cycle_b, vertex_a: int, vertex_b: int):
-    change = 0
-    cycle_a, cycle_b = list(cycle_a), list(cycle_b)
-    index_a, index_b = cycle_a.index(vertex_a), cycle_b.index(vertex_b)
-    # add new edges
-    change += matrix[vertex_b][cycle_a[index_a - 1]] + matrix[vertex_b][cycle_a[(index_a + 1) % len(cycle_a)]]
-    change += matrix[vertex_a][cycle_b[index_b - 1]] + matrix[vertex_a][cycle_b[(index_b + 1) % len(cycle_b)]]
-    # remove existing edges
-    change -= matrix[vertex_a][cycle_a[index_a - 1]] + matrix[vertex_a][cycle_a[(index_a + 1) % len(cycle_a)]]
-    change -= matrix[vertex_b][cycle_b[index_b - 1]] + matrix[vertex_b][cycle_b[(index_b + 1) % len(cycle_b)]]
-    return change
-
-def calculate_distance_diff_for_swap_inside_cycle(matrix, cycle, vertex_a: int, vertex_b: int):
-    change = 0
-    cycle = list(cycle)
-    index_a, index_b = cycle.index(vertex_a), cycle.index(vertex_b)
-    # edge case explained in images/obok.png
-    if abs(index_a - index_b) == 1:
-        change += matrix[vertex_b][cycle[index_a - 1]] + matrix[vertex_a][cycle[(index_b + 1) % len(cycle)]]
-        change -= matrix[vertex_b][cycle[index_a - 1]] + matrix[vertex_a][cycle[(index_b + 1) % len(cycle)]]
-    # edge case explained in images/konce.png
-    if index_a == 0 and index_b == len(cycle) - 1:
-        change += matrix[vertex_a][cycle[index_b - 1]] + matrix[vertex_b][cycle[index_a + 1]]
-        change -= matrix[vertex_a][cycle[index_a + 1]] + matrix[vertex_b][cycle[index_b - 1]]
-    else:
-        # add new edges
-        change += matrix[vertex_b][cycle[index_a - 1]] + matrix[vertex_b][cycle[(index_a + 1) % len(cycle)]]
-        change += matrix[vertex_a][cycle[index_b - 1]] + matrix[vertex_a][cycle[(index_b + 1) % len(cycle)]]
-        # remove existing edges
-        change -= matrix[vertex_a][cycle[index_a - 1]] + matrix[vertex_a][cycle[(index_a + 1) % len(cycle)]]
-        change -= matrix[vertex_b][cycle[index_b - 1]] + matrix[vertex_b][cycle[(index_b + 1) % len(cycle)]]
-    return change
-
-def calculate_distance_diff_for_swap_edges(matrix, cycle, vertex_a: int, vertex_b: int):
-    change = 0
-    cycle = list(cycle)
-    index_a, index_b = cycle.index(vertex_a), cycle.index(vertex_b)
-    # edge case for swapping edges between first and last nodes
-    if index_a == 0 and index_b == len(cycle) - 1:
-        change += matrix[vertex_a][cycle[index_b - 1]] + matrix[vertex_b][cycle[index_a + 1]]
-        change -= matrix[vertex_a][cycle[index_a + 1]] + matrix[vertex_b][cycle[index_b - 1]]
-    else:
-        change += matrix[vertex_a][cycle[(index_b + 1) % len(cycle)]] + matrix[vertex_b][cycle[index_a - 1]]
-        change -= matrix[vertex_a][cycle[index_a - 1]] + matrix[vertex_b][cycle[(index_b + 1) % len(cycle)]]
-    return change
-    
 
 if __name__ == "__main__":
 
@@ -111,50 +18,16 @@ if __name__ == "__main__":
 
     cycle_a, cycle_b = greedy_regret(matrix, vertices_kroa)
     cycle_a, cycle_b = cycle_a[:-1], cycle_b[:-1]
-
     print(cycle_a, cycle_b)
 
     length_a, length_b = calculate_path_length(matrix, cycle_a), calculate_path_length(matrix, cycle_b)
-
     print(length_a, length_b)
 
-    draw_cycles([cycle_a, cycle_b], vertices_kroa)
+    # draw_cycles([cycle_a, cycle_b], vertices_kroa)
 
-    # while True:
-    #     delta = swap_vertices_between_cycles(cycle_a, cycle_b)
-    #     print(delta)
-    #     if delta > 0:
-    #         break
+    steepest_search(matrix, cycle_a, cycle_b, "vertices")
 
-    # while True:
-    #     delta = swap_vertices_inside_cycle(cycle_a, cycle_b)
-    #     print(delta)
-    #     if delta > 0:
-    #         break
-    while True:
-        changes_a, changes_b = get_pairs_for_swap_inside_cycles(cycle_a, cycle_b)
-        a = np.array([calculate_distance_diff_for_swap_edges(matrix, cycle_a, changes_a[i][0], changes_a[i][1]) for i in range(len(changes_a))])
-        b = np.array([calculate_distance_diff_for_swap_edges(matrix, cycle_b, changes_b[i][0], changes_b[i][1]) for i in range(len(changes_b))])
-        print(np.min(a), np.min(b))
-        if np.min(a) == 0 and np.min(b) == 0:
-            break
-        if np.min(a) < 0:
-            vertex_a, vertex_b = changes_a[np.argmin(a)]
-            index_a, index_b = np.where(cycle_a == vertex_a)[0][0], np.where(cycle_a == vertex_b)[0][0]
-            if index_a == 0 and index_b == len(cycle_a) - 1:
-                cycle_a[index_a], cycle_a[index_b] = cycle_a[index_b], cycle_a[index_a]
-            cycle_a[index_a:index_b+1] = cycle_a[index_a:index_b+1][::-1]
-        if np.min(b) < 0:
-            vertex_a, vertex_b = changes_b[np.argmin(b)]
-            index_a, index_b = np.where(cycle_b == vertex_a)[0][0], np.where(cycle_b == vertex_b)[0][0]
-            if index_a == 0 and index_b == len(cycle_a) - 1:
-                cycle_b[index_a], cycle_b[index_b] = cycle_b[index_b], cycle_b[index_a]
-            cycle_b[index_a:index_b+1] = cycle_b[index_a:index_b+1][::-1]
+    length_a, length_b = calculate_path_length(matrix, cycle_a), calculate_path_length(matrix, cycle_b)
+    print(length_a, length_b)
 
-        length_a, length_b = calculate_path_length(matrix, cycle_a), calculate_path_length(matrix, cycle_b)
-
-        print(length_a, length_b)
-
-    draw_cycles([cycle_a, cycle_b], vertices_kroa)
-
-
+    # draw_cycles([cycle_a, cycle_b], vertices_kroa)
--- local_search/steepest_search.py
+++ local_search/steepest_search.py
@@ -1,30 +1,47 @@
 import numpy as np
 import swap_operations as so
+from collections import namedtuple
+
+from helpers import calculate_path_length
+
+Improvement = namedtuple("Improvement", ["change", "args", "function"])
 
 
-def steepest_search(matrix, cycle_a, cycle_b, swap_function):
+def steepest_search(matrix, cycle_a, cycle_b, inside_swap):
+
+    if inside_swap == "vertices":
+        swap_inside = so.swap_vertices_inside_cycle
+    else:
+        swap_inside = so.swap_edges_inside_cycle
     
     def between_cycles():
         pairs = so.get_pairs_for_swap_between_cycles(cycle_a, cycle_b)
         changes = np.array([so.calculate_distance_diff_for_swap_between_cycles(matrix, cycle_a, cycle_b, pairs[i][0], pairs[i][1]) for i in range(len(pairs))])
         if np.min(changes) >= 0:
-            return 0, None, None
-        vertex_a, vertex_b = np.argmin(changes)
-        return np.min(changes), (vertex_a, vertex_b), so.swap_vertices_between_cycles
+            return Improvement(0, None, None)
+        vertex_a, vertex_b = pairs[np.argmin(changes)]
+        return Improvement(np.min(changes), (cycle_a, cycle_b, vertex_a, vertex_b), so.swap_vertices_between_cycles)
     
     def inside_cycles():
         stats = [check_cycle(cycle) for cycle in [cycle_a, cycle_b]]
-        return stats[0] if stats[0][0] < stats[0][1] else stats[1]
+        return stats[0] if stats[0].change < stats[1].change else stats[1]
         
     def check_cycle(cycle):
         pairs = so.get_pairs_for_swap_inside_cycle(cycle)
-        changes = np.array([so.calculate_distance_diff_for_swap_inside_cycle(matrix, cycle_a, pairs[i][0], pairs[i][1]) for i in range(len(pairs))])
+        changes = np.array([so.calculate_distance_diff_for_swap_inside_cycle(matrix, cycle, pairs[i][0], pairs[i][1]) for i in range(len(pairs))])
         if np.min(changes) >= 0:
-            return 0, None ,None
-        vertex_a, vertex_b = np.argmin(changes)
-        return np.min(changes), (vertex_a, vertex_b), so.swap_vertices_inside_cycle
+            return Improvement(0, None, None)
+        vertex_a, vertex_b = pairs[np.argmin(changes)]
+        return Improvement(np.min(changes), (cycle, vertex_a, vertex_b), swap_inside)
     
     while True:
-        for func in [between_cycles, inside_cycles]:
-            change, vertices, swap_func = func()
-            
+        between = between_cycles()
+        inside = inside_cycles()
+        if between.change >= 0 and inside.change >= 0:
+            break
+        if between.change < inside.change:
+            print(f"Between: {calculate_path_length(matrix, cycle_a)}, {calculate_path_length(matrix, cycle_b)}, {between.change}")
+            between.function(*between.args)
+        else:
+            print(f"Inside: {calculate_path_length(matrix, cycle_a)}, {calculate_path_length(matrix, cycle_b)}, {inside.change}")
+            inside.function(*inside.args)
